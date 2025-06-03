b5c2ab276d0c57c249e40f9af0490d87bfaeb046
ankitalodha05 <76703320+ankitalodha05@users.noreply.github.com>
2025-06-03 15:34:43 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket1982"
+    bucket         = "my-terraform-state-bucket01062025"
     key            = "project-name/terraform.tfstate"
     region         = "ap-south-1"
     #dynamodb_table = "terraform-locks"
